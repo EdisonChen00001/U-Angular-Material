feb6eb531e37bd015ebea2f708dd09933543ff3a
zhouH0901 <zhezhou@my.bridgeport.edu>
2018-01-01 00:34:04 -0500

    19 working with component template

--- u-basics/src/app/servers/servers.component.ts
+++ u-basics/src/app/servers/servers.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: '<app-server></app-server> session 2 chapter 19<app-server></app-server>',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
