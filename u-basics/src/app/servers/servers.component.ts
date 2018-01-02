ddab7a31f6b41948c354db68190a427a24a17e4d
zhouH0901 <zhezhou@my.bridgeport.edu>
2018-01-01 20:49:05 -0500

    fully understanding the component selector

--- u-basics/src/app/servers/servers.component.ts
+++ u-basics/src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  //selector: '[app-servers]',
+  //selector:'.app-servers',
+  selector:'app-servers',
   template: '<app-server></app-server> session 2 chapter 19<app-server></app-server>',
   styleUrls: ['./servers.component.css']
 })
