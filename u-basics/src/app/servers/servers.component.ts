19e110cc3e7139787fdeda336d4a13e12ebda045
zhouH0901 <zhezhou@my.bridgeport.edu>
2018-01-01 20:58:16 -0500

    selector

--- u-basics/src/app/servers/servers.component.ts
+++ u-basics/src/app/servers/servers.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   //selector: '[app-servers]',
-  //selector:'.app-servers',
-  selector:'app-servers',
+  selector:'.app-servers',
+  //selector:'app-servers',
   template: '<app-server></app-server> session 2 chapter 19<app-server></app-server>',
   styleUrls: ['./servers.component.css']
 })
