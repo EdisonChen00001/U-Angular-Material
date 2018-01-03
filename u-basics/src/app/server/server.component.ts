1ef227acbfdf1abf35fd1b6c0be02aba77ac9cf6
zhouH0901 <zhezhou@my.bridgeport.edu>
2018-01-03 01:25:13 -0500

    Dynamiclly  with ngStyle

--- u-basics/src/app/server/server.component.ts
+++ u-basics/src/app/server/server.component.ts
@@ -1,15 +1,19 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-    selector:'app-server',
+    selector: 'app-server',
     templateUrl: './server.component.html'
 })
-export class ServerComponent{
+export class ServerComponent {
     serverId = 10;
-    serverStatus:string = 'offline'; 
+    serverStatus: string = 'offline';
 
 
-    getServerStatus(){
-        return this.serverStatus;
+    getServerStatus() {
+        return this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 }
